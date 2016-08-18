9621e40e198930ec5190a1901b099fcff8f3d942
blackendstudios <jeroflat@gmail.com>
2016-08-17 23:51:33 -0500

    feat(app): add query operators example
    we create queries from the command line to our database by using 'command-line-args' library, there
    
    we specify 'commandLineOptions' method to see fields that we want to query and the user types its
    
    values, that returns an 'options' object with values of the fields the user typed. There's other
    
    method called 'queryDocument' that actually creates our query document with the values specified
    
    from 'commandLineOptions' and that's use to retrieve the data we need.

--- projection.js
+++ projection.js
@@ -1,35 +1,72 @@
 var MongoClient = require('mongodb').MongoClient,
-   assert = require('assert');
+    commandLineArgs = require('command-line-args'), // give use a nice way of dealing with arguments pass to our application on the command line
+    assert = require('assert');
 
+var options = commandLineOptions();
 
 MongoClient.connect('mongodb://localhost:27017/crunchbase', function (err, db) {
 
    assert.equal(err, null);
    console.log("Successfully connected to MongoDB.");
 
-   var query = {"category_code": "biotech"},
-       projection = {"name": 1, "category_code": 1, "_id": 0};
+   var query = queryDocument(options),
+       projection = {"_id": 0, "name": 1, "founded_year": 1, "number_of_employees": 1, "crunchbase_url": 1};
 
-
-   var cursor = db.collection('companies').find(query);
-   /*
-      the current best practice in the nodejs driver is to chained a call to 'project' onto our cursor,
-      this call to 'project' sets a field projection for the query, this call not force a request for
-      retrieve documents from the database as does the 'forEach' method, rather it adds some additional
-      detail to the query representation maintained by our 'cursor', as we discuss before, there are a number
-      of cursor methods we can chain together to fully express the operation we wish execute against our database
-    */
-   cursor.project(projection);
+   var cursor = db.collection('companies').find(query, projection);
+   //cursor.project(projection);
+   var numMatches = 0;
 
    cursor.forEach(
       function (doc) {
-         //console.log(doc.name + " is a " + doc.category_code + " company.");
-         console.log(doc);
+         numMatches = numMatches + 1;
+         console.log( doc );
       },
       function (err) {
          assert.equal(err, null);
+         console.log("Our query was:" + JSON.stringify(query));
+         console.log("Matching documents: " + numMatches);
          return db.close();
       }
    );
 
 });
+
+function queryDocument(options) {
+   console.log(options);
+
+   var query = {
+      "founded_year": {
+         "$gte": options.firstYear,
+         "$lte": options.lastYear
+      }
+   };
+
+   if ("employees" in options) {
+      query.number_of_employees = {"$gte": options.employees};
+   }
+
+   return query;
+
+}
+
+function commandLineOptions() {
+
+   var cli = commandLineArgs([
+      {name: "firstYear", alias: "f", type: Number},
+      {name: "lastYear",  alias: "l", type: Number},
+      {name: "employees", alias: "e", type: Number}
+   ]);
+
+   var options = cli.parse(); // will parse everything that we've typed in the command line
+
+   if (!(("firstYear" in options) && ("lastYear" in options))) {
+      console.log(cli.getUsage({
+         title: 'Usage',
+         description: "The first two options are required"
+      }));
+      process.exit();
+   }
+
+   return options;
+
+}
